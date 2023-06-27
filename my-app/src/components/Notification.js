c8bd54766dbcc0b23ef9122994f550e274e8611e
faithzoe99 <zoezebedee1@gmail.com>
2023-06-27 17:31:20 +0100

    First Commit

---
+++ my-app/src/components/Notification.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Notification = ({ showNotification }) => {
+  <div className={`notification-container ${showNotification ? "show" : ""}`}>
+    <p>You have already entered this letter</p>
+  </div>;
+};
+
+export default Notification;
